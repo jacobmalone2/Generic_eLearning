5d2eb0e9a4de7db1931d5c404496ce80021e5c56
jacobmalone2 <143432329+jacobmalone2@users.noreply.github.com>
2025-02-03 16:57:43 -0700

    Added keys.

--- Models/Course.cs
+++ Models/Course.cs
@@ -5,6 +5,7 @@ namespace CS3750Assignment1.Models
 {
     public class Course
     {
+        [Key]
         public int Id { get; set; }
 
         [Required]
@@ -20,13 +21,19 @@ namespace CS3750Assignment1.Models
         public int CourseNumber { get; set; }
 
         [Required]
+        [Range(1, 80)]
         public int Credits { get; set; }
 
         [Required]
+        [Range(1,200)]
         public int Capacity { get; set; }
 
-        [Required]
-        public string MeetingTime { get; set; }
+        // List of the days of the week enum.
+        [StringLength(80, MinimumLength = 0)]
+        public List<DayOfWeek>? MeetingDays { get; set; }
+
+        [StringLength(15, MinimumLength = 0)]
+        public string? MeetingTime { get; set; }
 
         [StringLength(30, MinimumLength = 2)]
         [Required]

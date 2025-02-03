57aa803f23d5d67561d9f820ed018be1e7875a9d
jacobmalone2 <jacobmalone2@mail.weber.edu>
2025-02-02 21:41:43 -0700

    .

--- Pages/Courses/Index.cshtml.cs
+++ Pages/Courses/Index.cshtml.cs
@@ -25,8 +25,6 @@ namespace CS3750Assignment1.Pages.Courses
 
         public async Task OnGetAsync() 
         {
-            var cookieUserID = Request.Cookies["LoggedUserID"];
-
             instructorID = int.Parse(Request.Cookies["LoggedUserID"]);
 
             // Fetch courses only if ID is valid

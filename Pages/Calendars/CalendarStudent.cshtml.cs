4a923dfe16e8e9ef5e5c37c9cc6f4c1b9675ff8a
Carter Fausett <156369591+CarterFausett@users.noreply.github.com>
2025-02-06 22:08:23 -0700

    Scaffolded a CalendarInstructor and CalendarStudent page and fixed the weird RiverFevar typo in index.cshtml.cs

---
+++ Pages/Calendars/CalendarStudent.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CS3750Assignment1.Data;
+using CS3750Assignment1.Models;
+
+namespace CS3750Assignment1.Pages.Calendars
+{
+    public class CalendarStudentModel : PageModel
+    {
+        private readonly CS3750Assignment1.Data.CS3750Assignment1Context _context;
+
+        public CalendarStudentModel(CS3750Assignment1.Data.CS3750Assignment1Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Registration> Registration { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Registration = await _context.Registration
+                .Include(r => r.Account)
+                .Include(r => r.Course).ToListAsync();
+        }
+    }
+}

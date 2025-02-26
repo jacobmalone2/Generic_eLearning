0d6820ec566af8e3e045cd9f4ed7581956f251e5
jacobmalone2 <143432329+jacobmalone2@users.noreply.github.com>
2025-02-25 22:25:32 -0700

    Submissions are separated from assignments for students in their class.

--- Pages/Assignments/Index.cshtml.cs
+++ Pages/Assignments/Index.cshtml.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CS3750Assignment1.Data;
 using CS3750Assignment1.Models;
+using static CS3750Assignment1.Pages.Assignments.IndexModel;
+using static CS3750Assignment1.Pages.Registrations.IndexModel;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CS3750Assignment1.Pages.Assignments
 {
@@ -19,10 +22,12 @@ namespace CS3750Assignment1.Pages.Assignments
             _context = context;
         }
 
-        public IList<Assignment> Assignment { get; set; } = default!;
+        public IList<Assignment> Assignments { get; set; } = default!;
+        public IList<AssignmentViewModel> SubmittedAssignments { get; set; } = new List<AssignmentViewModel>();
+
         public int CourseID { get; private set; }
         public bool IsInstructor { get; private set; }
-        public HashSet<int> SubmittedAssignments { get; private set; } = new HashSet<int>();
+        public HashSet<int> submissions { get; private set; } = new HashSet<int>();
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -55,16 +60,65 @@ namespace CS3750Assignment1.Pages.Assignments
                 Expires = DateTime.Now.AddMinutes(180)
             });
 
-            Assignment = await _context.Assignment
-                .Where(a => a.CourseID == CourseID)
-                .ToListAsync();
+            // Get list of assignments.
+            var assignmentList = from a in _context.Assignment select a;
+            assignmentList = assignmentList.Where(a => a.CourseID == CourseID);
 
-            SubmittedAssignments = _context.Submission
-                .Where(s => s.StudentID == userId)
-                .Select(s => s.AssignmentID)
-                .ToHashSet();
+            // Check if student
+            if (!IsInstructor)
+            {
+                // Get list of submissions
+                // LINQ JOIN
+                var submissionList = (from a in _context.Assignment
+                                      join sub in _context.Submission
+                                      on a.Id equals sub.AssignmentID
+                                      where sub.StudentID == userId
+                                      select new AssignmentViewModel
+                                      {
+                                          Id = sub.Id,
+                                          StudentID = sub.StudentID,
+                                          CourseID = a.CourseID,
+                                          AssignmentID = a.Id,
+                                          Title = a.Title,
+                                          Description = a.Description,
+                                          EarnedPoints = sub.PointsEarned,
+                                          MaxPoints = a.MaxPoints,
+                                          SubmittedAt = sub.SubmittedAt
+                                      });
+
+                submissionList = submissionList.Where(s => s.StudentID == userId);
+
+                submissions = _context.Submission.Where(s => s.StudentID == userId)
+                .Select(s => s.AssignmentID).ToHashSet();
+
+                // Remove submissions from assignment list. Make Submissions their own list.
+                assignmentList = assignmentList.Where(s => !submissions.Contains(s.Id));
+                submissionList = submissionList.Where(s => (submissions.Contains(s.AssignmentID) && s.CourseID == CourseID));
+
+                // Finalize view data.
+                SubmittedAssignments = await submissionList.ToListAsync();
+            }
+
+
+            // Finalize view data.
+            Assignments = await assignmentList.ToListAsync();
 
             return Page();
         }
+    }
+
+    // Custom ViewModel to hold Registration and Course details
+    public class AssignmentViewModel
+    {
+        // New fields for Course information
+        public int Id { get; set; }
+        public int StudentID { get; set; }
+        public int CourseID { get; set; }
+        public int AssignmentID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? EarnedPoints { get; set; }
+        public int MaxPoints { get; set; }
+        public DateTime SubmittedAt { get; set; }
     }
 }
